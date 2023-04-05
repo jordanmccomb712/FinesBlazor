0d185ae696e8d9f177ec35ee6ccbba1598c85b37
Rachael Marshall <rachaelmarshall95@gmail.com>
2023-04-05 14:35:01 +0100

    adding validation to form

--- PaymentModel.cs
+++ PaymentModel.cs
@@ -1,17 +1,37 @@
 using System.ComponentModel.DataAnnotations;
+
+public class FromNowAttribute : ValidationAttribute
+    {
+        public FromNowAttribute() {}
+
+        public string GetErrorMessage() => "Date must be in future";
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var date = (DateTime)value;
+
+            if (DateTime.Compare(date, DateTime.Now) < 0) return new ValidationResult(GetErrorMessage());
+            else return ValidationResult.Success;
+        }
+    }
 public class PaymentModel
 
 {
+    [Required]
     public string? Title { get; set; }
+
+    [Required]
     public string? Name { get; set; }
-    public DateTime? Date { get; set; }
 
     [Required]
-    // [CreditCardAttribute]
-    public int? CardNum { get; set; }
+    [FromNow]
+    public DateTime? ExpDate { get; set; }
 
     [Required]
-    // [IntLength(3, ErrorMessage = "CVC must be 3 digits long.")]
-    public int? CVC { get; set; }
+    // [CreditCard]
+    public string? CardNum { get; set; }
+
+    [Required]
+    public string? CVC { get; set; }
     public string? FineID { get; set; }
 }
