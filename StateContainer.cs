bd35253e84631059098c21689894e7511814e480
jordanmccomb712 <jordanmccomb712@gmail.com>
2023-04-05 10:23:35 +0100

    added transitions between pages and dynamcially hiding elements

--- StateContainer.cs
+++ StateContainer.cs
@@ -1,8 +1,14 @@
 public class StateContainer
 {
     public static string testID {get; set;}
+    public static bool hiddenButton {get; set;} = true;
+    public static bool startButtonPushed {get; set;} = false;
+
+    public static bool foundUser {get; set;} = false; 
 
     private string? savedFineID;
+
+    public static Fine myFine {get; set;} = new Fine("xxxxxxxxxxxx", "place holder", 0, new DateTime(2021,12,31), false);
 
     public string Property
     {
