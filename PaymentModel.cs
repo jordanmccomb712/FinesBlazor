3fabbbee9446eb64976d0d505fbecbaef6dbe15e
Rachael Marshall <rachaelmarshall95@gmail.com>
2023-04-05 10:27:29 +0100

    payment changes

--- PaymentModel.cs
+++ PaymentModel.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 public class PaymentModel
+
 {
     public string? Title { get; set; }
     public string? Name { get; set; }
     public DateTime? Date { get; set; }
+
+    [Required]
+    // [CreditCardAttribute]
     public int? CardNum { get; set; }
+
+    [Required]
+    // [IntLength(3, ErrorMessage = "CVC must be 3 digits long.")]
     public int? CVC { get; set; }
     public string? FineID { get; set; }
 }
